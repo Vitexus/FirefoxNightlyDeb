85c91df051d4370bf0ff6ef4b7492ef340757736
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-06-25 23:07:09 +0200

    Try to build using jenkins

--- debian/mkico.sh
+++ debian/mkico.sh
@@ -3,14 +3,15 @@ PACKAGE="nightly"
 SRC="$PACKAGE.svg"
 DIR="debian/$PACKAGE/usr/share/icons/hicolor"
 
-resolutions='16 32 48 64 128 255 512'
+resolutions='16 32 48 64 128 256 512 1024'
 
 for resolution in $resolutions
 do
     echo $resolution
-    mkdir -p $DIR/${resolution}x${resolution}/
-    inkscape -z -w ${resolution} -h ${resolution} $SRC -o $DIR/${resolution}x${resolution}/$PACKAGE.png
+    mkdir -p $DIR/${resolution}x${resolution}/apps/
+    inkscape -z -w ${resolution} -h ${resolution} $SRC -o $DIR/${resolution}x${resolution}/apps/$PACKAGE.png
 done
-mkdir -p $DIR/scalable
-cp $SRC $DIR/scalable/$PACKAGE.svg 
+mkdir -p $DIR/scalable/apps
+cp $SRC $DIR/scalable/apps/$PACKAGE.svg 
 echo All done
+
